25a49e75f86fceeb3970f2e88a226ebd9c7dfa46
darkhandz <darkhandz0@gmail.com>
2025-06-02 16:40:21 +0800

    <fix>: 避免自定义编辑模式下的C-e和blink.cmp补全菜单隐藏冲突
    - 补全菜单隐藏的快捷键改为了C-;

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      ["<C-e>"] = {},
+      ["<C-;>"] = { "hide" },
+    },
+  },
+}
