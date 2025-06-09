40948a4479966c23dc87220e1b5861b386f2dd5d
jerry.chen <jerry.chen@richsound.com>
2025-06-09 10:24:48 +0800

    fzf-lua在grep的时候可以直接粘贴关键词

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -29,6 +29,13 @@ return {
         --删除默认的快捷键另外绑定一个,因为我有其他功能要绑这个组合键
         ["alt-l"] = { require("fzf-lua").actions.grep_lgrep },
         ["ctrl-g"] = false,
+        ["ctrl-v"] = function(selected, opts)
+          local fzf_lua = require("fzf-lua")
+          -- 获取剪贴板内容，如果为空则尝试获取默认寄存器内容
+          local clipboard = (vim.fn.getreg("+") ~= "" and vim.fn.getreg("+") or vim.fn.getreg('"')):gsub("[\r\n]", "")
+          -- 使用当前 fzf 窗口的 cwd 重新启动 grep 搜索
+          fzf_lua.grep({ search = clipboard, cwd = opts.cwd })
+        end,
       },
     },
   },
